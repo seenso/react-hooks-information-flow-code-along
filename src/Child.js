333c92e9baeba38d76ec99cc2d4f2265ceeaa7ae
seenso <>
2021-11-29 13:13:54 -0900

    Completed deliverable part 1

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor }) { //onChangeColor is the handleChangeColor function
+  return <div 
+      onClick={onChangeColor}
+      className="child" 
+      style={{ backgroundColor: "#FFF" }} 
+    />;
 }
 
 export default Child;
